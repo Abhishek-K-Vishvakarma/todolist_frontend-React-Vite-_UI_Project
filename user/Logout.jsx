26b6a887055fdb7abd50fe0488febe3c45c1ffba
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-11-01 23:21:31 +0530

    Uncommentde UserProfile and Logout code

--- user/Logout.jsx
+++ user/Logout.jsx
@@ -1,64 +1,64 @@
-// import { Container, Navbar } from "react-bootstrap";
-// import { Link, useNavigate } from "react-router-dom";
-// import Swal from "sweetalert2";
+import { Container, Navbar } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
-// const Logout = () => {
-//   const navigate = useNavigate();
+const Logout = () => {
+  const navigate = useNavigate();
 
-//   const LogoutUser = async () => {
-//     try {
-//       const res = await fetch("https://todolist-backend-node-js-apis-project.onrender.com/api/logout", {
-//         method: "POST",
-//         credentials: "include",
-//       });
-//       const data = await res.json();
-//       if (res.ok){
-//         Swal.fire({
-//           title: "Logout Successful!",
-//           html: "Redirecting to login page...",
-//           timer: 2000,
-//           timerProgressBar: true,
-//           didOpen: () => Swal.showLoading(),
-//         });
-//         setTimeout(() => navigate("/login"), 2000);
-//       } else {
-//         Swal.fire("Error", data.message || "Logout failed", "error");
-//       }
-//     } catch (err) {
-//       console.error("Internal server error:", err);
-//       Swal.fire("Error", "Something went wrong while logging out!", "error");
-//     }
-//   };
+  const LogoutUser = async () => {
+    try {
+      const res = await fetch("https://todolist-backend-node-js-apis-project.onrender.com/api/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (res.ok){
+        Swal.fire({
+          title: "Logout Successful!",
+          html: "Redirecting to login page...",
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => Swal.showLoading(),
+        });
+        setTimeout(() => navigate("/login"), 2000);
+      } else {
+        Swal.fire("Error", data.message || "Logout failed", "error");
+      }
+    } catch (err) {
+      console.error("Internal server error:", err);
+      Swal.fire("Error", "Something went wrong while logging out!", "error");
+    }
+  };
 
-//   return (
-//     <div style={{ backgroundColor: "#166C96", height: "56.96rem" }}>
-//       <Navbar style={{ backgroundColor: "#1b2651", color: "#edeae1" }}>
-//         <Container>
-//           <h4>
-//             <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
-//               Home
-//             </Link>
-//           </h4>
-//           <h4>Logout from your website</h4>
-//         </Container>
-//       </Navbar>
-//       <br />
-//       <div
-//         className="container card"
-//         style={{
-//           padding: "30px",
-//           backgroundColor: "#edeae1",
-//           boxShadow: "-3px 3px 3px 3px #1b2651",
-//         }}
-//       >
-//         <h4 className="text-center">Logout Now</h4>
-//         <br />
-//         <button onClick={LogoutUser} className="btn btn-danger">
-//           Logout
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div style={{ backgroundColor: "#166C96", height: "56.96rem" }}>
+      <Navbar style={{ backgroundColor: "#1b2651", color: "#edeae1" }}>
+        <Container>
+          <h4>
+            <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+              Home
+            </Link>
+          </h4>
+          <h4>Logout from your website</h4>
+        </Container>
+      </Navbar>
+      <br />
+      <div
+        className="container card"
+        style={{
+          padding: "30px",
+          backgroundColor: "#edeae1",
+          boxShadow: "-3px 3px 3px 3px #1b2651",
+        }}
+      >
+        <h4 className="text-center">Logout Now</h4>
+        <br />
+        <button onClick={LogoutUser} className="btn btn-danger">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
 
-// export default Logout;
+export default Logout;
--- user/UserProfile.jsx
+++ user/UserProfile.jsx
@@ -1,151 +1,151 @@
-// import { useState } from "react";
-// import { Container, Navbar } from "react-bootstrap"
-// import { Link } from "react-router-dom"
-// import { RiShieldUserFill } from "react-icons/ri";
-// import { FaRegEdit } from "react-icons/fa";
-// import Swal from "sweetalert2";
-// const UserProfile = () => {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [u, setU] = useState();
-//   const g = async () => {
-//     const res = await fetch("https://todolist-backend-node-js-apis-project.onrender.com/api/profile", {
-//       method: "GET",
-//       credentials: "include",
-//     });
+import { useState } from "react";
+import { Container, Navbar } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import { RiShieldUserFill } from "react-icons/ri";
+import { FaRegEdit } from "react-icons/fa";
+import Swal from "sweetalert2";
+const UserProfile = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [u, setU] = useState();
+  const g = async () => {
+    const res = await fetch("https://todolist-backend-node-js-apis-project.onrender.com/api/profile", {
+      method: "GET",
+      credentials: "include",
+    });
 
-//     const data = await res.json();
-//     if (data.status_code == 401) {
-//         window.location.href = "/login";
-//     }
-//     return data;
-//   };
-//   g().then((e) => setU(e?.user));    
-//   const SetUserData = () => {
-//     setName(u?.name);
-//     setEmail(u?.email);
-//   };
-//   const HandleSaveEditUserProfile = async (e) => {
-//     e.preventDefault();
-//     if (!name || !email) {
-//       let timerInterval;
-//       Swal.fire({
-//         title: "All fields are required to update your profile.",
-//         html: "I will close in <b></b> milliseconds.",
-//         timer: 2000,
-//         timerProgressBar: true,
-//         didOpen: () => {
-//           Swal.showLoading();
-//           const timer = Swal.getPopup().querySelector("b");
-//           timerInterval = setInterval(() => {
-//             timer.textContent = `${ Swal.getTimerLeft() }`;
-//           }, 100);
-//         },
-//         willClose: () => {
-//           clearInterval(timerInterval);
-//         }
-//       }).then((result) => {
-//         if (result.dismiss === Swal.DismissReason.timer) {
-//           console.log("I was closed by the timer");
-//         }
-//       });
-//       return;
-//     }
-//     try {
-//       const request = await fetch(`https://todolist-backend-node-js-apis-project.onrender.com/api/edituser/${u?._id}`, {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json"
-//         },
-//         credentials: 'include',
-//         body: JSON.stringify({ name: name, email: email })
-//       });
-//       const response = await request.json();
-//       if (!request.ok) {
-//         let timerInterval;
-//         Swal.fire({
-//           title: response.message,
-//           html: "I will close in <b></b> milliseconds.",
-//           timer: 2000,
-//           timerProgressBar: true,
-//           didOpen: () => {
-//             Swal.showLoading();
-//             const timer = Swal.getPopup().querySelector("b");
-//             timerInterval = setInterval(() => {
-//               timer.textContent = `${ Swal.getTimerLeft() }`;
-//             }, 100);
-//           },
-//           willClose: () => {
-//             clearInterval(timerInterval);
-//           }
-//         }).then((result) => {
-//           if (result.dismiss === Swal.DismissReason.timer) {
-//             console.log("I was closed by the timer");
-//           }
-//         });
-//       } else {
-//         let timerInterval;
-//         Swal.fire({
-//           title: response.message,
-//           html: "I will close in <b></b> milliseconds.",
-//           timer: 2000,
-//           timerProgressBar: true,
-//           didOpen: () => {
-//             Swal.showLoading();
-//             const timer = Swal.getPopup().querySelector("b");
-//             timerInterval = setInterval(() => {
-//               timer.textContent = `${ Swal.getTimerLeft() }`;
-//             }, 100);
-//           },
-//           willClose: () => {
-//             clearInterval(timerInterval);
-//           }
-//         }).then((result) => {
-//           if (result.dismiss === Swal.DismissReason.timer) {
-//             console.log("I was closed by the timer");
-//           }
-//         });
-//         setName("");
-//         setEmail("");
-//       }
-//       console.log(response);
-//     } catch (err) {
-//       console.log("Internal Server error :", err);
-//     }
-//   }
-//   return (
-//     <div style={{ backgroundColor: '#166C96', height: '56.96rem' }}>
-//       <Navbar style={{ backgroundColor: '#1b2651', color: '#edeae1' }}>
-//         <Container>
-//           <h4><Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>Home</Link></h4><h4>TODO List Website</h4>
-//         </Container>
-//       </Navbar>
-//       <div className="container card mt-5" style={{ padding: "30px", backgroundColor: "#edeae1", boxShadow: "-3px 3px 3px 3px #1b2651", maxWidth: "800px" }}>
-//         <div className="d-flex flex-column align-items-center">
-//           <div style={{ textAlign: "left", width: "100%" }}>
-//             <h3 className="text-center"><RiShieldUserFill className="fs-1" /> User Profile</h3>
-//             <p style={{ boxShadow: "-2px 2px 1px 5px #1b2651", borderRadius: '100%', width: '100%', height: '2px' }} />
-//             <p className="mb-2 fs-3"><strong>Full Name:</strong> {u?.name}</p>
-//             <p className="mb-0 fs-3"><strong>Email ID:</strong> {u?.email}</p>
-//             <p className="mb-0 fs-3"><strong>Role:</strong> {u?.role}</p>
-//             <p className="mb-0 fs-3"><strong>Gender:</strong> {u?.gender}</p>
-//           </div>
-//         </div>
-//       </div>
-//       <br />
-//       <div className="container card p-5" style={{ padding: "30px", backgroundColor: "#edeae1", boxShadow: "-3px 3px 3px 3px #1b2651", maxWidth: "800px" }}>
-//           <button onClick={SetUserData} style={{ background: 'none', fontSize: '20px', border: 'none', width: '30%'}} className="d-flex align-items-center gap-2" title="Set User Data in the input fields"><FaRegEdit />Edit Profile-Click</button>
-//         <form onSubmit={HandleSaveEditUserProfile}>
-//           <br />
-//           <label>FullName</label>
-//           <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" /><br />
-//           <label>Email-Id</label>
-//           <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" /><br />
-//           <button type="submit" className="btn btn-success w-100">Save</button>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
+    const data = await res.json();
+    if (data.status_code == 401) {
+        window.location.href = "/login";
+    }
+    return data;
+  };
+  g().then((e) => setU(e?.user));    
+  const SetUserData = () => {
+    setName(u?.name);
+    setEmail(u?.email);
+  };
+  const HandleSaveEditUserProfile = async (e) => {
+    e.preventDefault();
+    if (!name || !email) {
+      let timerInterval;
+      Swal.fire({
+        title: "All fields are required to update your profile.",
+        html: "I will close in <b></b> milliseconds.",
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const timer = Swal.getPopup().querySelector("b");
+          timerInterval = setInterval(() => {
+            timer.textContent = `${ Swal.getTimerLeft() }`;
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        }
+      }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+          console.log("I was closed by the timer");
+        }
+      });
+      return;
+    }
+    try {
+      const request = await fetch(`https://todolist-backend-node-js-apis-project.onrender.com/api/edituser/${u?._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: 'include',
+        body: JSON.stringify({ name: name, email: email })
+      });
+      const response = await request.json();
+      if (!request.ok) {
+        let timerInterval;
+        Swal.fire({
+          title: response.message,
+          html: "I will close in <b></b> milliseconds.",
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+              timer.textContent = `${ Swal.getTimerLeft() }`;
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          }
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+          }
+        });
+      } else {
+        let timerInterval;
+        Swal.fire({
+          title: response.message,
+          html: "I will close in <b></b> milliseconds.",
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+              timer.textContent = `${ Swal.getTimerLeft() }`;
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          }
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+          }
+        });
+        setName("");
+        setEmail("");
+      }
+      console.log(response);
+    } catch (err) {
+      console.log("Internal Server error :", err);
+    }
+  }
+  return (
+    <div style={{ backgroundColor: '#166C96', height: '56.96rem' }}>
+      <Navbar style={{ backgroundColor: '#1b2651', color: '#edeae1' }}>
+        <Container>
+          <h4><Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>Home</Link></h4><h4>TODO List Website</h4>
+        </Container>
+      </Navbar>
+      <div className="container card mt-5" style={{ padding: "30px", backgroundColor: "#edeae1", boxShadow: "-3px 3px 3px 3px #1b2651", maxWidth: "800px" }}>
+        <div className="d-flex flex-column align-items-center">
+          <div style={{ textAlign: "left", width: "100%" }}>
+            <h3 className="text-center"><RiShieldUserFill className="fs-1" /> User Profile</h3>
+            <p style={{ boxShadow: "-2px 2px 1px 5px #1b2651", borderRadius: '100%', width: '100%', height: '2px' }} />
+            <p className="mb-2 fs-3"><strong>Full Name:</strong> {u?.name}</p>
+            <p className="mb-0 fs-3"><strong>Email ID:</strong> {u?.email}</p>
+            <p className="mb-0 fs-3"><strong>Role:</strong> {u?.role}</p>
+            <p className="mb-0 fs-3"><strong>Gender:</strong> {u?.gender}</p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="container card p-5" style={{ padding: "30px", backgroundColor: "#edeae1", boxShadow: "-3px 3px 3px 3px #1b2651", maxWidth: "800px" }}>
+          <button onClick={SetUserData} style={{ background: 'none', fontSize: '20px', border: 'none', width: '30%'}} className="d-flex align-items-center gap-2" title="Set User Data in the input fields"><FaRegEdit />Edit Profile-Click</button>
+        <form onSubmit={HandleSaveEditUserProfile}>
+          <br />
+          <label>FullName</label>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" /><br />
+          <label>Email-Id</label>
+          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" /><br />
+          <button type="submit" className="btn btn-success w-100">Save</button>
+        </form>
+      </div>
+    </div>
+  )
+}
 
-// export default UserProfile;
+export default UserProfile;
